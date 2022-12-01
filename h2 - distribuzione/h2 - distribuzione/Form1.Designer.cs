df8a460843da3cf24f743c21958a25625fc5e96e
GianlucaG00 <gianlu.gambi@gmail.com>
2022-12-01 23:56:08 +0100

    distribution

---
+++ h2 - distribuzione/h2 - distribuzione/Form1.Designer.cs
@@ -0,0 +1,375 @@
+﻿namespace h2___distribuzione
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.GenerateBtn = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label11 = new System.Windows.Forms.Label();
+            this.amountNumbers = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.currAvg = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1091, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1091, 359);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(190, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(1091, 273);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(190, 31);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(1091, 191);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(190, 31);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(1091, 112);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(190, 31);
+            this.textBox5.TabIndex = 4;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(1091, 442);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(190, 31);
+            this.textBox6.TabIndex = 5;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(1091, 527);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(190, 31);
+            this.textBox7.TabIndex = 6;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(1091, 780);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(190, 31);
+            this.textBox8.TabIndex = 7;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(1091, 694);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(190, 31);
+            this.textBox9.TabIndex = 8;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(1091, 612);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(190, 31);
+            this.textBox10.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(892, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 39);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "0.0-0.09";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(892, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 39);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "0.10-0.19";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(892, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 39);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "0.20-0.29";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(892, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 39);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "0.30-0.39";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(892, 359);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 39);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "0.40-0.49";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(892, 442);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(113, 39);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "0.50-0.59";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(892, 527);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 39);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "0.60-0.69";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(892, 612);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(111, 39);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "0.70-0.79";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(892, 694);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(113, 39);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "0.80-0.89";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(892, 780);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(111, 39);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "0.90-0.99";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(90, 234);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(655, 577);
+            this.richTextBox1.TabIndex = 20;
+            this.richTextBox1.Text = "";
+            // 
+            // GenerateBtn
+            // 
+            this.GenerateBtn.Font = new System.Drawing.Font("Bahnschrift", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GenerateBtn.Location = new System.Drawing.Point(90, 126);
+            this.GenerateBtn.Name = "GenerateBtn";
+            this.GenerateBtn.Size = new System.Drawing.Size(193, 67);
+            this.GenerateBtn.TabIndex = 21;
+            this.GenerateBtn.Text = "Generate";
+            this.GenerateBtn.UseVisualStyleBackColor = true;
+            this.GenerateBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(80, 21);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(440, 58);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Univariate distribution";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // amountNumbers
+            // 
+            this.amountNumbers.Location = new System.Drawing.Point(622, 127);
+            this.amountNumbers.Name = "amountNumbers";
+            this.amountNumbers.Size = new System.Drawing.Size(126, 31);
+            this.amountNumbers.TabIndex = 23;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(413, 129);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(91, 29);
+            this.label12.TabIndex = 24;
+            this.label12.Text = "Amount:";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(413, 177);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(173, 29);
+            this.label13.TabIndex = 25;
+            this.label13.Text = "Current Average:";
+            // 
+            // currAvg
+            // 
+            this.currAvg.Location = new System.Drawing.Point(622, 175);
+            this.currAvg.Name = "currAvg";
+            this.currAvg.Size = new System.Drawing.Size(126, 31);
+            this.currAvg.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1596, 856);
+            this.Controls.Add(this.currAvg);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.amountNumbers);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.GenerateBtn);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button GenerateBtn;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox amountNumbers;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.TextBox currAvg;
+    }
+}
+
---
+++ h2 - distribuzione/h2 - distribuzione/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace h2___distribuzione
+{
+    public partial class Form1 : Form
+    {
+        List<double> list = new List<double>();
+        int[] counter = new int[] { 0,0,0,0,0,0,0,0,0,0};
+        int count = 0; 
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            double num = 0;
+
+
+            num = random.NextDouble();
+
+            if (num >= 0 && num < 0.1) textBox1.Text = (++counter[0]).ToString();
+            else if (num >= 0.1 && num < 0.2) textBox2.Text = (++counter[1]).ToString();
+            else if (num >= 0.2 && num < 0.3) textBox3.Text = (++counter[2]).ToString();
+            else if (num >= 0.3 && num < 0.4) textBox4.Text = (++counter[3]).ToString();
+            else if (num >= 0.4 && num < 0.5) textBox5.Text = (++counter[4]).ToString();
+            else if (num >= 0.5 && num < 0.6) textBox6.Text = (++counter[5]).ToString();
+            else if (num >= 0.6 && num < 0.7) textBox7.Text = (++counter[6]).ToString();
+            else if (num >= 0.7 && num < 0.8) textBox8.Text = (++counter[7]).ToString();
+            else if (num >= 0.8 && num < 0.9) textBox9.Text = (++counter[8]).ToString();
+            else if (num >= 0.9 && num < 1) textBox10.Text = (++counter[9]).ToString();
+
+            // calculate the average
+            list.Add(num);
+            double avg = list.Average();
+            this.richTextBox1.AppendText(num.ToString() + " ----> CURR_AVG: " + avg + "\n");
+
+            // print the amount of number generated
+            amountNumbers.Text = (++count).ToString();
+            currAvg.Text = avg.ToString(); 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(timer1.Enabled == false)
+            {
+                timer1.Enabled = true; 
+                GenerateBtn.Text = "Stop"; 
+            }
+            else
+            {
+                timer1.Enabled = false; 
+                GenerateBtn.Text = "Generate"; 
+            }
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
